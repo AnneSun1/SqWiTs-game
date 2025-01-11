4265fe0c23c5cb6a41f79a2db45516d4a1f9fe81
Julia Ilioukhina <jilioukh@uwaterloo.ca>
2025-01-11 11:30:34 -0500

    updated confidence interval

--- backend/OpenCVMonitoring.py
+++ backend/OpenCVMonitoring.py
@@ -21,12 +21,12 @@ def yolo_phone_detection():
             x1, y1, x2, y2, confidence, class_id = box[:6]
             label = model.names[int(class_id)]
 
-            if confidence > 0.5 and label == "cell phone":
+            if confidence > 0.7 and label == "cell phone":
                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
                 label_text = f'{label} {confidence:.2f}'
                 cv2.putText(frame, label_text, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        cv2.imshow("Phone detected", frame)
+        cv2.imshow("Dear Player, Masked Workers are Watching You...", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
