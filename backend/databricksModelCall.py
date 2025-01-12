3ec3961286ee9e46d5a8ca89b28652484e3b5c51
Julia Ilioukhina <jilioukh@uwaterloo.ca>
2025-01-12 01:20:47 -0500

    databricksModelCall

---
+++ backend/databricksModelCall.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import mlflow.sklearn
+
+load_dotenv()
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "databricks")
+DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
+DATABRICKS_TOKEN = os.getenv("DATABRICKS_TOKEN")
+MODEL_PATH = os.getenv("MLFLOW_MODEL_PATH")
+
+if not all([DATABRICKS_HOST, DATABRICKS_TOKEN, MODEL_PATH]):
+    raise ValueError("Missing required environment")
+
+os.environ["MLFLOW_TRACKING_URI"] = MLFLOW_TRACKING_URI
+os.environ["DATABRICKS_HOST"] = DATABRICKS_HOST
+os.environ["DATABRICKS_TOKEN"] = DATABRICKS_TOKEN
+
+try:
+    model = mlflow.sklearn.load_model(MODEL_PATH)
+except Exception as e:
+    print("Failed to load the model:", e)
+    exit()
+
+person_data = {
+    "age": 23,
+    "university": "University of Waterloo",
+    "exams_count": 5,
+    "gpa": 3.8
+}
+
+try:
+    user_df = pd.DataFrame([person_data])
+    user_df = pd.get_dummies(user_df, columns=["university"], drop_first=True)
+    missing_cols = set(model.feature_names_in_) - set(user_df.columns)
+    for col in missing_cols:
+        user_df[col] = 0
+    user_df = user_df[model.feature_names_in_]
+except Exception as e:
+    print("Failed to preprocess:", e)
+    exit()
+
+try:
+    survival_probability = model.predict_proba(user_df)[:, 1][0]
+    print(f"Predicted Survival Probability: {survival_probability * 100:.2f}%")
+except Exception as e:
+    print("Prediction failed:", e)
